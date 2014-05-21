fe2567e294860c0acd33e8662f3eac0adaa13457
Will Farley <will@django.nu>
2014-05-20 19:59:58 -0700

    adds facebookpost model and facebook forms

--- dtf/facebook_groups/managers.py
+++ dtf/facebook_groups/managers.py
@@ -11,6 +11,11 @@ class FBGroupManager(models.Manager):
 
 	def fb_create(self, **kwargs):
 		""" Creates a facebook group for the Facebook App
+		arguments:
+		name
+		description
+		privacy
+		user
 		"""
 		social_app = SocialApp.objects.all()[0]
 		client_id = social_app.client_id
@@ -42,3 +47,49 @@ class FBGroupManager(models.Manager):
 				user = User.objects.filter(is_superuser=True)[0]
 			new_group = self.create(fb_uid=new_group.json()["id"], owner=user)
 		return new_group
+
+
+class FBPostManager(models.Manager):
+
+	def fb_create(self, **kwargs):
+		""" Creates a facebook group for the Facebook App
+		arguments:
+		name
+		description
+		privacy
+		user
+		"""
+		social_app = SocialApp.objects.all()[0]
+		client_id = social_app.client_id
+		access_token = "{0}|{1}".format(client_id, social_app.secret)
+		user = None
+		fb_post = None
+
+		if kwargs.get('fb_uid'):
+			# if theres a fb_uid then were fetching an existing post
+			fb_uid = kwargs.get('fb_uid')
+			# create the beginning of the request url
+			fb_get_url = "https://graph.facebook.com/v2.0/{0}".format(fb_uid)
+			if kwargs.get("user"):
+				# if there's a user argument then use the user's acces token instead of the apps
+				user = kwargs.get('user')
+				access_token = user.get_fb_access_token().token
+			# format url with access token
+			fb_get_url = "{0}?access_token={1}".format(fb_get_url, access_token)
+			fb_post_request = requests.get(fb_get_url)
+			print fb_post_request
+			if fb_post_request.ok:
+				# if request 200 create a new FB post object in database
+				fb_post_json = fb_post_request.json()
+				fb_post = self.create(
+					fb_uid = fb_post_json['id'], 
+					from_user = fb_post_json['from'],
+					to_user = fb_post_json['to'],
+					message = fb_post_json['message'],
+					privacy = fb_post_json['privacy'],
+					actions = fb_post_json['actions'],
+					created_time = fb_post_json['created_time'],
+					updated_time = fb_post_json['updated_time'],
+					)
+			else: fb_post = fb_post_request
+		return fb_post
