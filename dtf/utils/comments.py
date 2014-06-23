b4bd11f6d678d78f67bd1d325a51b2aa65821536
goldhand <will@django.nu>
2014-06-23 14:07:12 -0700

    adds comment pagination

--- dtf/utils/comments.py
+++ dtf/utils/comments.py
@@ -1,6 +1,8 @@
-import django_comments
-
 from django.contrib.sites.models import get_current_site
+from django.contrib.contenttypes.models import ContentType
+
+import django_comments
+Comment = django_comments.get_model()
 
 
 def latest_comments(request):
@@ -8,9 +10,10 @@ def latest_comments(request):
     returns latest public comments from all models for the active site id
     '''
     site = get_current_site(request)
-    qs = django_comments.get_model().objects.filter(
+    content_type_id=ContentType.objects.get_for_model(Comment)
+    qs = Comment.objects.filter(
         site__pk = site.pk,
         is_public = True,
         is_removed = False,
-    )
+    ).exclude(content_type=content_type_id) # exclude comment's comments
     return qs.order_by('-submit_date')[:40] 
