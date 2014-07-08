9779a42f61220d4ad3220f21a69676ca4b4acc63
goldhand <will@django.nu>
2014-07-07 19:02:36 -0600

    syncs infusionsoft packages on save

--- dtf/packages/models.py
+++ dtf/packages/models.py
@@ -10,6 +10,18 @@ from jsonfield import JSONField
 # from packages.providers.infusionsoft import server, key
 from .managers import InfusionsoftTagManager, PackagePurchaseManager
 
+def remove_unused(_dict):
+    return_dict = {}
+    for _key, _value in _dict.iteritems():
+        if _value:
+            return_dict[_key] = _value
+    return return_dict
+
+def setdictattrs(obj, _dict):
+    _dict = remove_unused(_dict)
+    for _key, _value in _dict.iteritems():
+        setattr(obj, _key, _value)
+
 
 class Package(models.Model):
     """
@@ -20,7 +32,7 @@ class Package(models.Model):
     lessons = models.ManyToManyField("lessons.Lesson")
 
     def __unicode__(self):
-        return u'{}'.format(self.name)
+        return u'{}'.format(self.name if self.name else 'Package')
 
 
 class PackagePurchase(models.Model):
@@ -57,57 +69,86 @@ class InfusionsoftPackage(Package):
     """
     Package with infusionsoft api hooks
     """
-    remote_id = models.TextField(blank=True)
-    product_id = models.TextField(blank=True)
-    cycle = models.TextField(blank=True)
-    frequency = models.TextField(blank=True)
-    pre_authorize_amount = models.TextField(blank=True)
-    prorate = models.TextField(blank=True)
-    active = models.TextField(blank=True)
-    plan_price = models.TextField(blank=True)
-    action_set_id = models.TextField(blank=True)
+    subscription_id = models.TextField(blank=True, null=True)
+    product_id = models.TextField(blank=True, null=True)
+    cycle = models.TextField(blank=True, null=True)
+    frequency = models.TextField(blank=True, null=True)
+    pre_authorize_amount = models.TextField(blank=True, null=True)
+    prorate = models.TextField(blank=True, null=True)
+    active = models.TextField(blank=True, null=True)
+    plan_price = models.TextField(blank=True, null=True)
+    product_price = models.TextField(blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    status = models.TextField(blank=True, null=True)
+    action_set_id = models.TextField(blank=True, null=True)
     tag = models.OneToOneField("InfusionsoftTag", blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        sync_data = self._get_sync_data(product_id=self.product_id) if self.product_id else None
+        if sync_data:
+            setdictattrs(self, sync_data)
+        return super(InfusionsoftPackage, self).save(*args, **kwargs)
 
     def sync(self):
         sync_data = self._get_sync_data()
         if sync_data:
-            self = InfusionsoftPackage(**sync_data)
-            self.save()
+            setdictattrs(self, sync_data)
+            
+        self.save()
 
-    def _get_sync_data(self):
-        provider_data = self._get_provider_data()
-        product_data = self._get_product_data()
-        if provider_data:
+    def _get_sync_data(self, product_id=None):
+        subscription_data = self._get_subscription_data(product_id)
+        product_data = self._get_product_data(product_id)
+        if subscription_data and product_data:
             package_data = dict({
                 "id": self.id,
                 "pk": self.pk,
                 "action_set_id": self.action_set_id,
-                "name": self.name,
-                "remote_id": provider_data["Id"],
-                "product_id": provider_data["ProductId"],
-                "cycle": provider_data["Cycle"],
-                "frequency": provider_data["Frequency"],
-                "prorate": provider_data["Prorate"],
-                "active": provider_data["Active"],
-                "plan_price": provider_data["PlanPrice"]
+                "name": product_data.get("ProductName"),
+                "subscription_id": subscription_data.get("Id"),
+                "product_id": subscription_data.get("ProductId"),
+                "cycle": subscription_data.get("Cycle"),
+                "frequency": subscription_data.get("Frequency"),
+                "prorate": subscription_data.get("Prorate"),
+                "active": subscription_data.get("Active"),
+                "plan_price": subscription_data.get("PlanPrice"),
+                "product_price": product_data.get("ProductPrice"),
+                "description": product_data.get("Description"),
+                "status": product_data.get("Status"),
                 })
-            return package_data
+        elif product_data:
+            # product but not subscription
+            package_data = dict({
+                "id": self.id,
+                "pk": self.pk,
+                "action_set_id": self.action_set_id,
+                "name": product_data.get("ProductName"),
+                "product_id": product_data.get("Id"),
+                "product_price": product_data.get("ProductPrice"),
+                "description": product_data.get("Description"),
+                "status": product_data.get("Status"),
+                })
 
-    def _get_provider_data(self):
-        if self.product_id:
+
+        return package_data if package_data else None
+
+    def _get_subscription_data(self, product_id=None):
+        product_id = product_id if product_id else self.product_id
+        if product_id:
             results = server.DataService.findByField(key, "SubscriptionPlan",
-                10, 0, "productid", self.product_id,
+                10, 0, "productid", product_id,
                 ["Id", "ProductId", "Cycle", "Frequency", "PreAuthorizeAmount",
                 "Prorate", "Active", "PlanPrice"]);
-            return results[0]
+            return results[0] if len(results) else None
 
-    def _get_product_data(self):
-        if self.product_id:
+    def _get_product_data(self, product_id=None):
+        product_id = product_id if product_id else self.product_id
+        if product_id:
             results = server.DataService.findByField(key, "Product",
-                10, 0, "id", self.product_id,
-                ["Id", "ProductName", "productPrice", "ShortDescription", "Description",
-                "LargeImage", "Status", "IsPackage"]);
-            return results[0]
+                10, 0, "id", product_id,
+                ["Id", "ProductName", "ProductPrice", "Description", 
+                "Status", "IsPackage"]);
+            return results[0] if len(results) else None
 
     def cancel_subscription(self, contactId, actionSetId):
         results = server.ContactService.runActionSequence(key, contactId,
@@ -128,7 +169,7 @@ class InfusionsoftTag(models.Model):
     objects = InfusionsoftTagManager()
 
     def __unicode__(self):
-        return self.group_name if self.group_name else u'InfusionsoftTag Object'
+        return u'{}'.format(self.group_name if self.group_name else u'InfusionsoftTag Object')
 
     def save(self, *args, **kwargs):
         remote_id = kwargs.get('remote_id') if kwargs.get('remote_id') else self.remote_id
@@ -152,10 +193,10 @@ class InfusionsoftTag(models.Model):
             tag_data = dict({
                 "id": self.id,
                 "pk": self.pk,
-                "remote_id": provider_data["Id"],
-                "group_category_id": provider_data["GroupCategoryId"],
-                "group_name": provider_data["GroupName"],
-                "group_description": provider_data["GroupDescription"],
+                "remote_id": provider_data.get("Id"),
+                "group_category_id": provider_data.get("GroupCategoryId"),
+                "group_name": provider_data.get("GroupName"),
+                "group_description": provider_data.get("GroupDescription"),
                 })
             return tag_data
 
@@ -165,4 +206,4 @@ class InfusionsoftTag(models.Model):
             results = server.DataService.findByField(key, "ContactGroup",
                 10, 0, "id", remote_id,
                 ["Id", "GroupCategoryId", "GroupName", "GroupDescription"]);
-            return results[0]
+            return results[0] if len(results) else None
