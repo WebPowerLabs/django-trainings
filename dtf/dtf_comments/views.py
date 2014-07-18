89a06141da3d3ecc969e81f5d0f60b16bd81f6b6
Andrew Sutulov <andrew.sutulov@gmail.com>
2014-07-18 17:09:46 +0300

    Issue #151

--- dtf/dtf_comments/views.py
+++ dtf/dtf_comments/views.py
@@ -3,15 +3,42 @@ from django.contrib.sites.models import Site
 from django.views.generic.base import View
 from django.contrib.auth.decorators import login_required
 from django.http.response import HttpResponse
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, render_to_response
 from django.conf import settings
 from ipware.ip import get_real_ip, get_ip
 
 from courses.models import Content
 from dtf_comments.models import DTFComment
 from dtf_comments.templatetags.markdown import markdown
-from utils.views import CreateFormBaseView, AjaxResponsePermissionMixin
+from utils.views import (CreateFormBaseView, AjaxResponsePermissionMixin,
+                         PermissionMixin)
 from dtf_comments.forms import DTFCommentShareForm
+from django_comments.views.moderation import perform_delete
+from django_comments.views.utils import next_redirect
+from django import template
+
+
+class CommentDeleteView(PermissionMixin, View):
+    decorators = {'GET': login_required, 'POST': login_required}
+
+    def get(self, request, *args, **kwargs):
+        comment = get_object_or_404(DTFComment, pk=self.kwargs['comment_id'],
+                                    site__pk=settings.SITE_ID)
+        next_url = request.GET.get('next', None)
+        return render_to_response('comments/delete.html',
+                                  {'comment': comment, 'next': next_url},
+                                  template.RequestContext(request))
+
+    def post(self, request, *args, **kwargs):
+        comment = get_object_or_404(DTFComment, pk=self.kwargs['comment_id'],
+                                    site__pk=settings.SITE_ID)
+        if request.user.is_staff:
+            perform_delete(request, comment)
+        elif request.user == comment.user:
+            comment.is_removed = True
+            comment.save()
+        return next_redirect(request, fallback=next or 'comments-delete-done',
+                             c=comment.pk)
 
 
 class CommentPreviewView(AjaxResponsePermissionMixin, View):
