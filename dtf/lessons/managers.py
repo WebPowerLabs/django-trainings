4a0486db7526b7d6729ba5ec079d2c53d4368a56
Andrew Sutulov <andrew.sutulov@gmail.com>
2014-07-24 23:49:33 +0300

    Issues #156 #157 #158

--- dtf/lessons/managers.py
+++ dtf/lessons/managers.py
@@ -36,38 +36,41 @@ class LessonManager(PolymorphicManager):
                 return self.all()
         return self.published()
 
-    def get_next_url(self, obj, tag_id=None, course_id=None, user=None):
+    def get_next_url(self, obj, tag_id=None, course_id=None, purchased=None,
+                     user=None):
         """
         Receive Lesson instance and returns URL to next Lesson object.
         """
         lessons = self.get_list(user)
+        if purchased:
+            lessons = self.purchased(user)
         if tag_id:
-            lessons = lessons.filter(tags=tag_id,
-                                     created__lt=obj.created).order_by(
-                                                              '-created')[:1]
+            lessons = lessons.filter(tags=tag_id, created__lt=obj.created
+                                     ).order_by('-created')[:1]
         elif course_id:
-            lessons = lessons.filter(course_id=course_id,
-                                _order__gt=obj._order).order_by('_order')[:1]
+            lessons = lessons.filter(course_id=course_id, _order__gt=obj._order
+                                     ).order_by('_order')[:1]
         else:
-            lessons = lessons.filter(created__lt=obj.created).order_by(
-                                                              '-created')[:1]
+            lessons = lessons.filter(created__lt=obj.created
+                                     ).order_by('-created')[:1]
         if lessons:
             return reverse('lessons:detail', kwargs={'slug': lessons[0].slug})
         return None
 
-    def get_prev_url(self, obj, tag_id=None, course_id=None, user=None):
+    def get_prev_url(self, obj, tag_id=None, course_id=None, purchased=None,
+                     user=None):
         """
         Receive Lesson instance and returns URL to previous Lesson object.
         """
         lessons = self.get_list(user)
+        if purchased:
+            lessons = self.purchased(user)
         if tag_id:
-            lessons = lessons.filter(tags=tag_id,
-                                     created__gt=obj.created).order_by(
-                                                                'created')[:1]
+            lessons = lessons.filter(tags=tag_id, created__gt=obj.created
+                                     ).order_by('created')[:1]
         elif course_id:
-            lessons = lessons.filter(course_id=course_id,
-                                       _order__lt=obj._order).order_by(
-                                                                '-_order')[:1]
+            lessons = lessons.filter(course_id=course_id, _order__lt=obj._order
+                                     ).order_by('-_order')[:1]
         else:
             lessons = lessons.filter(created__gt=obj.created).order_by(
                                                                 'created')[:1]
