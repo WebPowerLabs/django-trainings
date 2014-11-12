9e15e52d4227246d030619ba994b9f13bc8052a4
goldhand <will@django.nu>
2014-11-11 17:04:55 -0800

    removes fb group migrations

--- dtf/facebook_groups/migrations/0001_initial.py
+++ dtf/facebook_groups/migrations/0001_initial.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-import datetime
+from south.utils import datetime_utils as datetime
 from south.db import db
 from south.v2 import SchemaMigration
 from django.db import models
@@ -14,17 +14,45 @@ class Migration(SchemaMigration):
             ('fb_uid', self.gf('django.db.models.fields.CharField')(unique=True, max_length=255)),
             ('owner', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['users.User'])),
             ('name', self.gf('django.db.models.fields.CharField')(max_length=255, blank=True)),
+            ('description', self.gf('django.db.models.fields.TextField')(blank=True)),
             ('venue', self.gf('django.db.models.fields.TextField')(blank=True)),
             ('privacy', self.gf('django.db.models.fields.CharField')(max_length=255, blank=True)),
             ('icon', self.gf('django.db.models.fields.URLField')(max_length=255, blank=True)),
             ('email', self.gf('django.db.models.fields.EmailField')(max_length=255, blank=True)),
+            ('feed', self.gf('django.db.models.fields.TextField')(blank=True)),
+            ('pinned_post', self.gf('django.db.models.fields.related.OneToOneField')(to=orm['facebook_groups.FacebookPost'], unique=True, null=True, blank=True)),
+            ('pinned_comment', self.gf('django.db.models.fields.related.OneToOneField')(to=orm['dtf_comments.DTFComment'], unique=True, null=True, blank=True)),
+            ('thumbnail', self.gf('django.db.models.fields.files.ImageField')(max_length=100, null=True, blank=True)),
+            ('thumbnail_height', self.gf('django.db.models.fields.CharField')(max_length=255, blank=True)),
+            ('thumbnail_width', self.gf('django.db.models.fields.CharField')(max_length=255, blank=True)),
+            ('cover', self.gf('django.db.models.fields.files.ImageField')(max_length=100, null=True, blank=True)),
+            ('cover_height', self.gf('django.db.models.fields.CharField')(max_length=255, blank=True)),
+            ('cover_width', self.gf('django.db.models.fields.CharField')(max_length=255, blank=True)),
+            ('active', self.gf('django.db.models.fields.BooleanField')(default=True)),
         ))
         db.send_create_signal(u'facebook_groups', ['FacebookGroup'])
+
+        # Adding model 'FacebookPost'
+        db.create_table(u'facebook_groups_facebookpost', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('fb_uid', self.gf('django.db.models.fields.CharField')(unique=True, max_length=255)),
+            ('from_user', self.gf('jsonfield.fields.JSONField')(default={}, blank=True)),
+            ('to_user', self.gf('jsonfield.fields.JSONField')(default={}, blank=True)),
+            ('message', self.gf('django.db.models.fields.TextField')(blank=True)),
+            ('actions', self.gf('django.db.models.fields.TextField')(blank=True)),
+            ('privacy', self.gf('django.db.models.fields.TextField')(blank=True)),
+            ('created_time', self.gf('django.db.models.fields.TextField')(blank=True)),
+            ('updated_time', self.gf('django.db.models.fields.TextField')(blank=True)),
+        ))
+        db.send_create_signal(u'facebook_groups', ['FacebookPost'])
 
 
     def backwards(self, orm):
         # Deleting model 'FacebookGroup'
         db.delete_table(u'facebook_groups_facebookgroup')
+
+        # Deleting model 'FacebookPost'
+        db.delete_table(u'facebook_groups_facebookpost')
 
 
     models = {
@@ -48,16 +76,78 @@ class Migration(SchemaMigration):
             'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
         },
+        u'courses.content': {
+            'Meta': {'object_name': 'Content'},
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'id': ('django.db.models.fields.CharField', [], {'max_length': '36', 'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'owner': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['users.User']"}),
+            'polymorphic_ctype': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "u'polymorphic_courses.content_set'", 'null': 'True', 'to': u"orm['contenttypes.ContentType']"}),
+            'published': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'slug': ('django_extensions.db.fields.AutoSlugField', [], {'allow_duplicates': 'False', 'max_length': '50', 'separator': "u'-'", 'blank': 'True', 'populate_from': "'name'", 'overwrite': 'False'}),
+            'updated': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'})
+        },
+        u'django_comments.comment': {
+            'Meta': {'ordering': "('submit_date',)", 'object_name': 'Comment', 'db_table': "'django_comments'"},
+            'comment': ('django.db.models.fields.TextField', [], {'max_length': '3000'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'content_type_set_for_comment'", 'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'ip_address': ('django.db.models.fields.GenericIPAddressField', [], {'max_length': '39', 'null': 'True', 'blank': 'True'}),
+            'is_public': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_removed': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'object_pk': ('django.db.models.fields.TextField', [], {}),
+            'site': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['sites.Site']"}),
+            'submit_date': ('django.db.models.fields.DateTimeField', [], {'default': 'None'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'comment_comments'", 'null': 'True', 'to': u"orm['users.User']"}),
+            'user_email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'user_name': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'}),
+            'user_url': ('django.db.models.fields.URLField', [], {'max_length': '200', 'blank': 'True'})
+        },
+        u'dtf_comments.dtfcomment': {
+            'Meta': {'ordering': "('submit_date',)", 'object_name': 'DTFComment', '_ormbases': [u'django_comments.Comment']},
+            u'comment_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['django_comments.Comment']", 'unique': 'True', 'primary_key': 'True'}),
+            'hero_unit': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['courses.Content']", 'null': 'True', 'blank': 'True'})
+        },
         u'facebook_groups.facebookgroup': {
             'Meta': {'object_name': 'FacebookGroup'},
+            'active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'cover': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'cover_height': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            'cover_width': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
             'email': ('django.db.models.fields.EmailField', [], {'max_length': '255', 'blank': 'True'}),
             'fb_uid': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'}),
+            'feed': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
             'icon': ('django.db.models.fields.URLField', [], {'max_length': '255', 'blank': 'True'}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
             'owner': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['users.User']"}),
+            'pinned_comment': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['dtf_comments.DTFComment']", 'unique': 'True', 'null': 'True', 'blank': 'True'}),
+            'pinned_post': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['facebook_groups.FacebookPost']", 'unique': 'True', 'null': 'True', 'blank': 'True'}),
             'privacy': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            'thumbnail': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'thumbnail_height': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            'thumbnail_width': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
             'venue': ('django.db.models.fields.TextField', [], {'blank': 'True'})
+        },
+        u'facebook_groups.facebookpost': {
+            'Meta': {'object_name': 'FacebookPost'},
+            'actions': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'created_time': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'fb_uid': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'}),
+            'from_user': ('jsonfield.fields.JSONField', [], {'default': '{}', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'message': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'privacy': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'to_user': ('jsonfield.fields.JSONField', [], {'default': '{}', 'blank': 'True'}),
+            'updated_time': ('django.db.models.fields.TextField', [], {'blank': 'True'})
+        },
+        u'sites.site': {
+            'Meta': {'ordering': "(u'domain',)", 'object_name': 'Site', 'db_table': "u'django_site'"},
+            'domain': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
         },
         u'users.user': {
             'Meta': {'object_name': 'User'},
@@ -72,6 +162,7 @@ class Migration(SchemaMigration):
             'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
             'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
             'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'phone': ('localflavor.us.models.PhoneNumberField', [], {'max_length': '20'}),
             'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Permission']"}),
             'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
         }
